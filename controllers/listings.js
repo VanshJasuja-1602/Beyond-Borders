407af48b6a643b28770b63d85ddfea7fcd5bea41
Vansh Jasuja <vanshjasuja394@gmail.com>
2026-02-25 23:39:54 +0530

    Final production fixes

--- controllers/listings.js
+++ controllers/listings.js
@@ -26,7 +26,9 @@ module.exports.showListing = async (req, res) => {
     req.flash("error", "Listing you requested for does not exist!");
     return res.redirect("/listings");
   }
-  res.render("listings/show.ejs", { listing });
+  res.render("listings/show.ejs", { listing,
+    mapToken:process.env.MAP_TOKEN
+   });
 }
 
 module.exports.createListing = async (req, res, next) => {
