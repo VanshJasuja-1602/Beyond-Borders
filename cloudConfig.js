f5faf2926e25c97e1bd83ab75913598d297632f1
Vansh Jasuja <vanshjasuja394@gmail.com>
2026-02-25 21:12:28 +0530

    Fix CloudinaryStorage params format

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,18 +1,19 @@
 const cloudinary = require('cloudinary').v2;
-const multerStorageCloudinary = require('multer-storage-cloudinary');
-const CloudinaryStorage = multerStorageCloudinary.CloudinaryStorage;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET
+  api_secret: process.env.CLOUD_API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
-  params: {
-    folder: 'Beyond_Border_DEV',
-    allowed_formats: ["png", "jpg", "jpeg"],
+  params: async (req, file) => {
+    return {
+      folder: 'Beyond_Border_DEV',
+      allowed_formats: ['png', 'jpg', 'jpeg'],
+    };
   },
 });
 
